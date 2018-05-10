ab24da061419f1a97d683a81034ef064916b3191
NishantGhanate <nishant7.ng@gmail.com>
2018-05-10 20:37:12 +0530

    Docs create demo

---
+++ Docs/Docs.py
@@ -0,0 +1,38 @@
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='Intense Quote')
+
+document.add_paragraph(
+    'first item in unordered list', style='List Bullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='List Number'
+)
+
+#document.add_picture('monty-truth.png', width=Inches(1.25))
+recordset = ['1','2','3']
+table = document.add_table(rows=1, cols=3)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+hdr_cells[2].text = 'Desc'
+for item in recordset:
+    row_cells = table.add_row().cells
+    row_cells[0].text = item
+    row_cells[1].text = item
+    row_cells[2].text = item
+
+document.add_page_break()
+
+document.save('demo.docx')
