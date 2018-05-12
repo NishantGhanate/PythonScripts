ee011c2b6d4ab0e169f3f7a950a2ee0fc005fc43
nishantghanate <nishant7.ng@gmail.com>
2018-05-13 00:03:04 +0530

    File rename

--- Docs/ReadDocument.py
+++ Docs/ReadDocument.py
@@ -1,7 +1,9 @@
 from docx import Document
 
+#ReadBinary mode
 f = open('demo.docx','rb')
 document = Document(f)
+#append file
 p = document.add_paragraph('B plain paragraph having some ')
 document.save('demo2.docx')
 f.close()
