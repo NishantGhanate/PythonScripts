6a38101ec65aefbce863c3e16b645f7370195470
nishantghanate <nishant7.ng@gmail.com>
2018-08-03 22:00:14 +0530

    Socket initial commit

---
+++ SocketPrograms/simpleServer.py
@@ -0,0 +1,13 @@
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print ('Got connection from', addr)
+   c.send('Thank you for connecting')
+   c.close()                # Close t
