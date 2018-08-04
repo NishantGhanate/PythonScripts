793d35b37a1be1fa43c0fe73baa6d1a8e614e507
nishantghanate <nishant7.ng@gmail.com>
2018-08-04 13:51:47 +0530

    Socket working { enable hosting service & internet info service }

--- SocketPrograms/simpleClient.py
+++ SocketPrograms/simpleClient.py
@@ -2,7 +2,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+port = 8080                # Reserve a port for your service.
 
 s.connect((host, port))
 print (s.recv(1024))
--- SocketPrograms/simpleServer.py
+++ SocketPrograms/simpleServer.py
@@ -1,9 +1,16 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
+print(s)
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+print(host)
+
+port = 8080                # Reserve a port for your service.
+
+
+
 s.bind((host, port))        # Bind to the port
+
 
 s.listen(5)                 # Now wait for client connection.
 while True:
