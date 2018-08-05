1200bc0278011a05a7774f652796f6300fbf8527
nishantghanate <nishant7.ng@gmail.com>
2018-08-05 15:35:49 +0530

    Filtering DB

--- Django/MyDjangoApp/music/models.py
+++ Django/MyDjangoApp/music/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=191)
+    album_title = models.CharField(max_length=191)
+    genre = models.CharField(max_length=85)
+    album_logo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete = models.CASCADE)
+    file_type = models.CharField(max_length = 10 )
+    song_title = models.CharField(max_length = 191 )
+    
