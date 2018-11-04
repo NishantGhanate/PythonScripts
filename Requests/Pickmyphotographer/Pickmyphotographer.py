849907b43f9dc4509648460983a04cf54ddb6f78
nishantghanate <nishant7.ng@gmail.com>
2018-11-04 11:56:01 +0530

    Pickphoto final commit

--- Requests/Pickmyphotographer/Pickmyphotographer.py
+++ Requests/Pickmyphotographer/Pickmyphotographer.py
@@ -1,6 +1,7 @@
 from requests_html import HTMLSession
 import requests.exceptions 
 import time
+import csv
 
 # ToDo collect email , address ,contactnumber, name , city name. https://www.pickmyphotographer.com/mumbai
 
@@ -13,25 +14,51 @@ def getUrl(url):
     # session = HTMLSession()
     global session
     b = session.get(url)
-    src = b.html.xpath("//p[@itemprop='address']//text()") # get address of company 
-    print(src)
-    src = b.html.xpath("//a[@target='_blank']/@href") # get urls of company 
-    print(src)
-    src = b.html.xpath("//div[@class='price-value']//text()") # get prices 
-    print(src)
+    address = b.html.xpath("//p[@itemprop='address']//text()") # get address of company 
+    print(address)
+    cUrls = b.html.xpath("//a[@target='_blank']/@href") # get urls of company 
+    print(cUrls)
+    price = b.html.xpath("//div[@class='price-value']//text()") # get prices 
+    print(price)
+    row = [address,cUrls,price]
+    with open('Pick.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(row)
+
 
 url = 'https://www.pickmyphotographer.com/mumbai'
+# session = HTMLSession()
+# r = session.get(url)
+# print(r.html.absolute_links)
+
+# xpathSrc = "//h2[@class='profile-title']/a/@href"
+# src = r.html.xpath(xpathSrc)
+
+with open('Pick.csv', 'w') as csvfile:
+    fieldnames = ['Address', 'Url' , 'Price']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+
+
 session = HTMLSession()
-r = session.get(url)
-print(r.html.absolute_links)
 
-xpathSrc = "//h2[@class='profile-title']/a/@href"
-src = r.html.xpath(xpathSrc)
+for i in range(15):
+    url = url + "#all/page-" + str(i)
+    r = session.get(url)
+    xpathSrc = "//h2[@class='profile-title']/a/@href"
+    src = r.html.xpath(xpathSrc)
+    for s in src:
+        s = 'https:'+s
+        print(s)
+        getUrl(s)
+        time.sleep(5)
 
-for s in src:
-    s = 'https:'+s
-    print(s)
-    getUrl(s)
+
+# for s in src:
+#     s = 'https:'+s
+#     print(s)
+#     getUrl(s)
    
 
 # ---------------------------  Testting work   -----------------------------------
