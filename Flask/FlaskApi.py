5b230c71134458e7b4a1835d9ab88d935181bee8
nishantghanate <nishant7.ng@gmail.com>
2018-12-31 18:43:40 +0530

    Flask Init

---
+++ Flask/FlaskApi.py
@@ -0,0 +1,22 @@
+from flask import Flask , request
+from flask_restful import Resource , Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Hello(Resource):
+    def get(self):
+        return {'about' : 'Hello programmer !'}
+    def post(self):
+        postData = request.json()
+        return {'You have sent ' : postData}
+
+class Math(Resource):
+    def get(self,num):
+        return {'num' : num * 10}
+
+api.add_resource(Hello,'/')
+api.add_resource(Math,'/math/<int:num>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
