5c4fdb7fd3b2f5ed385a4d3b5c46065ed10114b7
NishantGhanate <nishant7.ng@gmail.com>
2019-09-07 19:58:46 +0530

    Asyncio added

---
+++ Async/Readme.md
@@ -0,0 +1,6 @@
+#Visualitation : sync vs async
+
+> pip install asyncio
+
+![](https://github.com/NishantGhanate/PythonScripts/Async/image.jpeg)
+
--- Firebase/Smart.py
+++ Firebase/Smart.py
@@ -5,20 +5,20 @@ from firebase_admin import firestore
 from firebase_admin import storage as fb_storage
 
 import os 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/Nishant/Documents/AdminSdk.json"
-# set GOOGLE_APPLICATION_CREDENTIALS= "C:/Users/Nishant/Documents/AdminSdk.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="H:/Github/FinalYearProject/Desktop/Config/Service.json"
+
+
 class Firebase():
     def __init__(self):
         # Fetch the service account key JSON file contents
-    #     cred = credentials.Certificate('C:/Users/Nishant/Documents/AdminSdk.json')
-    #     # As an admin, the app has access to read and write all data, regradless of Security Rules
-    #     firebase_admin.initialize_app(cred, {
-    #     'databaseURL': 'https://smart-security-383a4.firebaseio.com/',
-    #     'storageBucket': 'gs://smart-security-383a4.appspot.com'
-    # })
-        print("")
-         
-    
+        cred = credentials.Certificate('C:/Users/Nishant/Documents/AdminSdk.json')
+        # As an admin, the app has access to read and write all data, regradless of Security Rules
+        firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://smartsecurity-38229.firebaseio.com/',
+        'storageBucket': 'gs://smartsecurity-38229.appspot.com'
+    })
+    print("")
+       
     def PostFireStore(self):
        # Add a new document
         db = firestore.Client()
@@ -35,7 +35,7 @@ class Firebase():
     def PostImageFireStore(self):
        # Add a new document
         db = firestore.Client()
-        doc_ref = db.collection(u'users').document(u'Images')
+        doc_ref = db.collection(u'testing').document(u'Images')
         file = open('C:/Users/Nishant/Pictures/Spolier.png', 'rb')
         blob = file.read()
         file.close()
