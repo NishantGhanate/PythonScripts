757a20772fae40917904980a1c87fda2f82d995e
NishantGhanate <nishant7.ng@gmail.com>
2020-04-02 14:52:36 +0530

    Cython added

---
+++ Cython/SingleScript/Something.py
@@ -0,0 +1,12 @@
+class Somthing:
+
+    def __init__(self):
+        print('Class loaded')
+
+    def waddup(self):
+        print('Cython is cool')
+
+if __name__ == "__main__":
+    something = Somthing()
+    something.waddup()
+    
